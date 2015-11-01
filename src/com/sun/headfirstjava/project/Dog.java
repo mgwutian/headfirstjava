7e69a807ec42703b40bd96b1005802a83cc31ee4
mgwutian <mgwutian@163.com>
2015-11-01 12:42:00 +0800

    my first commit

---
+++ src/com/sun/headfirstjava/project/Dog.java
@@ -0,0 +1,10 @@
+package com.sun.headfirstjava.project;
+
+/**
+ * Created by sxm on 2015/10/31.
+ */
+public class Dog {
+    public static void main(String [] args){
+        System.out.println("my first class for head first java");
+    }
+}
